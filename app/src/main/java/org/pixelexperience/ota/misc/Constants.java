87dc11809cddac260f621a3970380fe2ef4bad32
Omansh Krishn <96343317+omansh-krishn@users.noreply.github.com>
2023-08-02 10:13:12 +0530

    modify constants

--- app/src/main/java/org/pixelexperience/ota/misc/Constants.java
+++ app/src/main/java/org/pixelexperience/ota/misc/Constants.java
@@ -30,9 +30,9 @@ public final class Constants {
     static final String PROP_AB_DEVICE = "ro.build.ab_update";
     static final String PROP_DEVICE = "org.pixelexperience.device";
     static final String PROP_BUILD_VERSION = "org.pixelexperience.version";
-    static final String OTA_URL = "https://api.pixelexperience.org/ota/%s/%s";
-    static final String OTA_CI_URL = "https://api.pixelexperience.org/ota_ci/%s/%s";
-    static final String MAINTAINER_URL = "https://download.pixelexperience.org/team/%s";
-    static final String DOWNLOAD_WEBPAGE_URL = "https://download.pixelexperience.org/changelog/%s/%s";
+    static final String OTA_URL = "https://gitlab.com/androidsantoni/ota/pixelexperience/-/raw/%s/%s";
+    static final String OTA_CI_URL = "https://gitlab.com/androidsantoni/ota/pixelexperience/-/raw/%s/%s";
+    static final String MAINTAINER_URL = "https://t.me/omansh_krishn";
+    static final String DOWNLOAD_WEBPAGE_URL = "https://gitlab.com/androidsantoni/ota/pixelexperience/-/raw/%s/changelog/%s";
     static final String EXPORT_PATH = "PixelExperience-Updates/";
 }
